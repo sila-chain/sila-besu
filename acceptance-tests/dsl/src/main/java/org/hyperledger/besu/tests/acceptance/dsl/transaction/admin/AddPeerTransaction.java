f6edfaae369af7a0875e1bcab523746f9258d84e
SILA Network <pssd500@gmail.com>
2026-09-26 01:09:17 +0300

    identity: use Sila Web3j namespace in acceptance DSL
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/admin/AddPeerTransaction.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/admin/AddPeerTransaction.java
@@ -22,7 +22,7 @@ import org.hyperledger.besu.tests.acceptance.dsl.transaction.Transaction;
 import java.io.IOException;
 import java.net.URI;
 
-import org.web3j.protocol.core.Response;
+import sila.web3j.protocol.core.Response;
 
 public class AddPeerTransaction implements Transaction<Boolean> {
 
