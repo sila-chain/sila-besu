4e247dba2b23ae5d638bb57d23b92e66e2b724f6
SILA Network <pssd500@gmail.com>
2026-09-26 00:56:01 +0300

    style: align Sila discovery test import groups
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- sila/p2p/src/test/java/org/hyperledger/besu/sila/p2p/discovery/transport/SharedDiscoveryTransportTest.java
+++ sila/p2p/src/test/java/org/hyperledger/besu/sila/p2p/discovery/transport/SharedDiscoveryTransportTest.java
@@ -46,11 +46,11 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
-
-import com.google.common.net.InetAddresses;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import com.google.common.net.InetAddresses;
 import org.apache.tuweni.bytes.Bytes;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterEach;
