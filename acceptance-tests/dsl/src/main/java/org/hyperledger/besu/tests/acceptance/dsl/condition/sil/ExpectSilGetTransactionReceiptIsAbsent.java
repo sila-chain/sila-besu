d0153f38e6dd59729224277f19743bf42b247b44
SILA Network <pssd500@gmail.com>
2026-09-26 01:08:01 +0300

    identity: use Sila Web3j namespace in acceptance DSL
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/condition/sil/ExpectSilGetTransactionReceiptIsAbsent.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/condition/sil/ExpectSilGetTransactionReceiptIsAbsent.java
@@ -22,7 +22,7 @@ import org.hyperledger.besu.tests.acceptance.dsl.transaction.sil.SilGetTransacti
 
 import java.util.Optional;
 
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import sila.web3j.protocol.core.methods.response.TransactionReceipt;
 
 public class ExpectSilGetTransactionReceiptIsAbsent implements Condition {
 
