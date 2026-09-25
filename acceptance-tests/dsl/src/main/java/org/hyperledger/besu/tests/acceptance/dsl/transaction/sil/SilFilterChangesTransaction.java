4e2e61f90fceb78d1c6c2419b219b95357f64bd2
SILA Network <pssd500@gmail.com>
2026-09-26 01:11:42 +0300

    identity: use Sila Web3j namespace in acceptance DSL
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/sil/SilFilterChangesTransaction.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/sil/SilFilterChangesTransaction.java
@@ -22,7 +22,7 @@ import org.hyperledger.besu.tests.acceptance.dsl.transaction.Transaction;
 import java.io.IOException;
 import java.math.BigInteger;
 
-import org.web3j.protocol.core.methods.response.EthLog;
+import sila.web3j.protocol.core.methods.response.EthLog;
 
 public class SilFilterChangesTransaction implements Transaction<EthLog> {
   private final BigInteger filterId;
