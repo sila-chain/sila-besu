81451ba3490df289181935ad1342fe327bf6aefd
SILA Network <pssd500@gmail.com>
2026-09-26 01:10:18 +0300

    identity: use Sila Web3j namespace in acceptance DSL
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/net/NetPeerCountTransaction.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/net/NetPeerCountTransaction.java
@@ -22,7 +22,7 @@ import org.hyperledger.besu.tests.acceptance.dsl.transaction.Transaction;
 import java.io.IOException;
 import java.math.BigInteger;
 
-import org.web3j.protocol.core.methods.response.NetPeerCount;
+import sila.web3j.protocol.core.methods.response.NetPeerCount;
 
 public class NetPeerCountTransaction implements Transaction<BigInteger> {
 
