cec919e44d3f41c50806adef6f5a1d296e47c3d6
SILA Network <pssd500@gmail.com>
2026-09-26 14:18:10 +0300

    style: match Sila P2P import groups
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- sila/p2p/src/main/java/org/hyperledger/besu/sila/p2p/discovery/discv5/PeerDiscoveryAgentFactoryV5.java
+++ sila/p2p/src/main/java/org/hyperledger/besu/sila/p2p/discovery/discv5/PeerDiscoveryAgentFactoryV5.java
@@ -14,12 +14,6 @@
  */
 package org.hyperledger.besu.sila.p2p.discovery.discv5;
 
-import java.net.InetSocketAddress;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import org.apache.tuweni.bytes.Bytes;
-import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.crypto.SECPPublicKey;
 import org.hyperledger.besu.crypto.SignatureAlgorithm;
 import org.hyperledger.besu.crypto.SignatureAlgorithmFactory;
@@ -40,6 +34,14 @@ import org.hyperledger.besu.sila.p2p.peers.Peer;
 import org.hyperledger.besu.sila.p2p.permissions.PeerPermissions;
 import org.hyperledger.besu.sila.p2p.rlpx.RlpxAgent;
 import org.hyperledger.besu.sila.storage.StorageProvider;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sila.beacon.discovery.AddressAccessPolicy;
